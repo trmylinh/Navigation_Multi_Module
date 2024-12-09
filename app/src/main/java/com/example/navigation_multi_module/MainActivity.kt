8f2affbc761f690bacb8444aacbc3e6021681556
LinhTM_Android <linhtm@infinitytech.global>
2024-12-09 11:33:56 +0700

    init: add module

---
+++ app/src/main/java/com/example/navigation_multi_module/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.navigation_multi_module
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Navigation_Multi_Module"
+include(":app")
+include(":questions")
+include(":result")
